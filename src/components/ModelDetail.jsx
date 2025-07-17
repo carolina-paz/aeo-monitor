0a20a496fee0c3533b7d5ad325e7b80fd4095ac7
Carolina Ruz <kro.ruz@gmail.com>
2025-07-17 18:56:12 -0400

    mobile

--- src/components/ModelDetail.jsx
+++ src/components/ModelDetail.jsx
@@ -10,35 +10,33 @@ const ModelDetail = ({ model, ranking, brandName}) => {
 
     if (ranking.length === 0) {
         return (
-            <div className="flex flex-col gap-2  p-4 rounded-lg">
-                <div className="text-gray-800 text-2xl bg-white text-blue-900 border-2 border-blue-900 p-2 rounded-lg  text-center w-full font-bold">
+            <div className="flex flex-col gap-2 p-2 md:p-4 rounded-lg">
+                <div className="text-gray-800 text-lg md:text-2xl bg-white text-blue-900 border-2 border-blue-900 p-2 rounded-lg text-center w-full font-bold">
                     {model}
                 </div>
             </div>
         );
     }
     return (
-      <div className="flex flex-col gap-2  p-4 rounded-lg">
-          <div className="text-gray-800 text-2xl bg-white text-blue-900 border-2 border-blue-900 p-2 rounded-lg  text-center w-full font-bold">
+      <div className="flex flex-col gap-2 p-2 md:p-4 rounded-lg">
+          <div className="text-gray-800 text-lg md:text-2xl bg-white text-blue-900 border-2 border-blue-900 p-2 rounded-lg text-center w-full font-bold">
             <div>
                 {model}
             </div>
-          <div className="text-gray-800 text-2xl bg-white text-blue-900  p-2 rounded-lg  text-center w-full font-bold">
+            <div className="text-gray-800 text-lg md:text-2xl bg-white text-blue-900 p-2 rounded-lg text-center w-full font-bold">
                 <div className={brandPosition !== "No presente" ? "text-green-600" : "text-red-600"}>{brandPosition !== "No presente" ? `#${brandPosition}` : brandPosition}</div>
-          </div>
-          <p onClick={() => setShowRanking(!showRanking)} className=" underline text-sm cursor-pointer">{showRanking ? "Ocultar ranking" : "Ver ranking completo"}</p>
-          {showRanking && ranking.length > 0 && (
-                <div className="flex flex-col gap-2 items-start text-left px-1 mt-2">
+            </div>
+            <p onClick={() => setShowRanking(!showRanking)} className="underline text-xs md:text-sm cursor-pointer">{showRanking ? "Ocultar ranking" : "Ver ranking completo"}</p>
+            {showRanking && ranking.length > 0 && (
+                <div className="flex flex-col gap-1 md:gap-2 items-start text-left px-1 mt-2">
                     {ranking.map((item, index) => (
-                        <p key={index} className={`${index >= 3 ? 'text-sm' : 'text-lg'} truncate w-full`}>
+                        <p key={index} className={`${index >= 3 ? 'text-xs md:text-sm' : 'text-sm md:text-lg'} truncate w-full`}>
                             {index + 1}. {item}
                         </p>
                     ))}
                 </div>
             )}
           </div>
-
-
       </div>
     );
   };
--- src/components/QuestionBlock.jsx
+++ src/components/QuestionBlock.jsx
@@ -5,18 +5,16 @@ import { findPosition } from '../utils';
 const QuestionBlock = ({ question,  ranking, brandName}) => { //ranking is an array of arrays ranking
   return (
     <div className="flex flex-col gap-2 bg-[#1E2938] p-4 rounded-lg shadow-xl" >
-        <div className="text-white text-2xl font-bold">
+        <div className="text-white text-lg md:text-2xl font-bold">
             {question}
         </div>
-<div className="flex justify-between    ">
-    {Models.map((model) => (
-        <div className="w-1/4">
-
-            <ModelDetail model={model} ranking={ranking} brandName={brandName} />
+        <div className="flex flex-col md:flex-row md:justify-between gap-4">
+            {Models.map((model) => (
+                <div key={model} className="w-full md:w-1/4">
+                    <ModelDetail model={model} ranking={ranking} brandName={brandName} />
+                </div>
+            ))}
         </div>
-    ))}
-</div>
-
     </div>
   );
 };
